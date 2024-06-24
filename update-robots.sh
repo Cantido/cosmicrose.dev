1874b21419e7bb18b0cc7e384939d346a1c34b22
Rosa Richter <cosmic.lady.rosa@gmail.com>
2024-06-24 13:04:36 -0600

    Add update-robots.sh to generate a new robots.txt

---
+++ update-robots.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+curl "https://api.darkvisitors.com/robots-txts" \
+  -X POST \
+  -H "Authorization: Bearer ${DARKVISITORS_TOKEN:?Error DARKVISITORS_TOKEN variable must be set}" \
+  -H "Content-Type: application/json" \
+  --data @darkvisitors.json \
+  -o public/robots.txt
