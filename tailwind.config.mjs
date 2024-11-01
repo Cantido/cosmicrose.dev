72ec6a785a5cf05cbfa7be3c498fa61b30a1590e
Rosa Richter <cosmic.lady.rosa@gmail.com>
2024-11-01 15:40:38 -0600

    Revert "Use Libertinus font cuz its nice"
    This reverts commit 58a95b758a80bc26e2b4f05728fdd06b22a4b57e.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {
-      fontFamily: {
-        serif: [
-          'Libertinus Serif',
-          ...defaultTheme.fontFamily.serif,
-        ],
-        display: 'Libertinus Serif Display, ui-serif',
-        initials: 'Libertinus Serif Initials, ui-serif',
-      }
-    },
+		extend: {},
 	},
 	plugins: [
     require('daisyui'),
