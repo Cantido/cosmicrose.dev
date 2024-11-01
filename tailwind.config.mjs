58a95b758a80bc26e2b4f05728fdd06b22a4b57e
Rosa Richter <cosmic.lady.rosa@gmail.com>
2024-11-01 15:29:31 -0600

    Use Libertinus font cuz its nice

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      fontFamily: {
+        serif: [
+          'Libertinus Serif',
+          ...defaultTheme.fontFamily.serif,
+        ],
+        display: 'Libertinus Serif Display, ui-serif',
+        initials: 'Libertinus Serif Initials, ui-serif',
+      }
+    },
 	},
 	plugins: [
     require('daisyui'),
